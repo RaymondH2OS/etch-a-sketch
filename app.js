6d340a896ceaa712ed558b9745602f21ee7bbdd9
Raymond Waters <raymondh2os@gmail.com>
2022-07-10 13:44:13 -0700

    Tools are now functional

--- app.js
+++ app.js
@@ -1,13 +1,44 @@
-const grid = document.querySelector(".container");
+const grid = document.querySelector(".grid");
 const clear = document.querySelector("#clear");
 const changeGrid = document.querySelector("#gridSize");
-var slider = document.getElementById("rangeInput");
+const slider = document.getElementById("rangeInput");
+const showGridSize = document.getElementById("showGridSize");
+const cells = document.querySelector(".cell");
+const colorBtn = document.getElementById("color");
+const eraserBtn = document.getElementById("eraser");
+const randomBtn = document.getElementById("random");
+const colorPicker = document.getElementById("colorPicker");
 
-clear.addEventListener('click', clearGrid);
-changeGrid.addEventListener('click', newGrid);
+var defaultGridSize = 16;
 
-createGrid(16);
+const DEFAULT_MODE = "color";
+const DEFAULT_COLOR = "black";
 
+let currentMode = DEFAULT_MODE;
+let currentColor = DEFAULT_COLOR;
+
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
+clear.addEventListener("click", clearGrid);
+changeGrid.addEventListener("click", newGrid);
+
+colorBtn.onclick = () => setCurrentMode("color");
+randomBtn.onclick = () => setCurrentMode("random");
+eraserBtn.onclick = () => setCurrentMode("eraser");
+colorPicker.oninput = (e) => setCurrentColor(e.target.value);
+
+function setCurrentMode(newMode) {
+  activateButton(newMode);
+  currentMode = newMode;
+}
+
+function setCurrentColor(newColor) {
+  currentColor = newColor;
+}
+
+// Function to create grid
 function createGrid(gridSize) {
   grid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr`;
   grid.style.gridTemplateRows = `repeat(${gridSize}, 1fr`;
@@ -15,21 +46,65 @@ function createGrid(gridSize) {
   for (let i = 0; i < size; i++) {
     const div = document.createElement("div");
     div.classList.add("cell");
-    grid.appendChild(div);
     div.addEventListener("mouseover", draw);
+    div.addEventListener("mousedown", draw);
+    grid.appendChild(div);
   }
 }
 
+// Function for drawing on grid
 function draw(e) {
-  e.target.style.backgroundColor = "black";
+  if (e.type === "mouseover" && !mouseDown) return;
+  e.preventDefault();
+  if (currentMode === "random") {
+    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    color = "#" + randomColor;
+    e.target.style.backgroundColor = color;
+  } else if (currentMode === "color") {
+    e.target.style.backgroundColor = currentColor;
+  } else if (currentMode === "eraser") {
+    e.target.style.backgroundColor = "white";
+  }
 }
 
+// Function to clear grid
 function clearGrid() {
   var cells = grid.querySelectorAll("div");
   cells.forEach((cell) => (cell.style.backgroundColor = "white"));
 }
 
+// Function to generate new grid
 function newGrid() {
   document.querySelectorAll(".cell").forEach((e) => e.remove());
   createGrid(slider.value);
 }
+
+// Function to show current grid size
+function gridSize(value) {
+  showGridSize.textContent = `${value}x${value}`;
+}
+
+// Function for which color to use based on button click
+function activateButton(newMode) {
+  if (currentMode === "random") {
+    randomBtn.classList.remove("active");
+  } else if (currentMode === "color") {
+    colorBtn.classList.remove("active");
+  } else if (currentMode === "eraser") {
+    eraserBtn.classList.remove("active");
+  }
+
+  if (newMode === "random") {
+    randomBtn.classList.add("active");
+  } else if (newMode === "color") {
+    colorBtn.classList.add("active");
+  } else if (newMode === "eraser") {
+    eraserBtn.classList.add("active");
+  }
+}
+
+window.onload = () => {
+  createGrid(defaultGridSize);
+  gridSize(defaultGridSize);
+  activateButton(DEFAULT_MODE);
+};
